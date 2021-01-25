cc3fed957051521888c3c7e9b23e4e5f6942f1ab
Jimmy Zhen <jzhen@stanford.edu>
2021-01-25 00:35:57 -0800

    Initial commits for linters and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "react-app/jest",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  "plugins": [
+    "prettier"
+  ],
+  "rules": {
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {"devDependencies": true}
+    ],
+    "react/jsx-props-no-spreading": "off"
+  },
+  "env": {
+    "browser": true,
+    "jest": true
+  }
+};
