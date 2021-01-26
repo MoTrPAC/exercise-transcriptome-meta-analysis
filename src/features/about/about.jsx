50a30fe0d41251a4b2dc39a4fa687b37344d41cd
Jimmy Zhen <jzhen@stanford.edu>
2021-01-26 11:53:22 -0800

    Initial commit of about feature with placeholder dummy text

---
+++ src/features/about/about.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about-page-container">
+      {/* About the consortium */}
+      <section className="page-section">
+        <div className="container">
+          <div className="row text-left">
+            <h2 className="section-heading">About this site</h2>
+            <p className="text-muted">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default About;
