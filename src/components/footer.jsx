91d1a8a551008a1ddfeb1962cc16f998033708f1
Jimmy Zhen <jzhen@stanford.edu>
2021-01-26 11:52:24 -0800

    Initial commit of footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <section className="footer-container py-3 bg-dark w-100">
+      <div className="container">
+        <div className="footer-content text-center">
+          <span>Built with ❤️ by Stanford University</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
