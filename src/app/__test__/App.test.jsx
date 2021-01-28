fd9de81ed81c8efe713bde31ba0d7b69a36552b1
Jimmy Zhen <jzhen@stanford.edu>
2021-01-28 00:01:23 -0800

    Updated generic app test assertion

--- src/app/__test__/App.test.jsx
+++ src/app/__test__/App.test.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
+import configureStore from '../configureStore';
+import App from '../App';
 
-test('renders learn react link', () => {
+const store = configureStore();
+
+test('renders app name text', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Exercise Transcriptome Meta-analysis/i)).toBeInTheDocument();
 });
