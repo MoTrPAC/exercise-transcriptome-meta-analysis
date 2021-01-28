85c8c84d0bc654a4e8f33bf5d62afe9cfaaf003d
Jimmy Zhen <jzhen@stanford.edu>
2021-01-28 00:10:18 -0800

    Replaced boilerplate code with app-specific code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,56 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import AnalysisConnected from '../features/analysis/analysis';
+import About from '../features/about/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="header-container">
+          <Header />
+        </header>
+        <section className="main">
+          <Switch>
+            <Route path="/" exact component={AnalysisConnected} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </section>
+        <footer className="footer mt-auto">
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
